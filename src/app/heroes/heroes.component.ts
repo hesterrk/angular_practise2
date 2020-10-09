955f485d118b86eec646a00e8ed981bdb7368524
Hester Knight <hester2knight@mac.com>
2020-10-09 11:30:04 +0100

    hero displays with selected hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HEROES } from '../mock-heroes';
 
 // The selector name: the unique name used to identify the app component in the HTML DOM tree once it is rendered into the browser
 // selector property of the component is just a string that is used to identify the component or an element in the DOM
@@ -10,12 +11,18 @@ import { Hero } from './hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  //heroes is our property that holds our mock data
+  heroes = HEROES;
+
+  // Selected hero has the type Hero
+  selectedHero: Hero;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  // This method assigns the clicked hero from the template to the component's selectedHero
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
